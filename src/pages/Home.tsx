fa0ec2bc1cad628b40327097270155276627209a
Reiniger95 <reiniger.95@gmail.com>
2021-07-14 21:28:41 -0300

    feat: :fire: Initial commit
    First commit, half of project

---
+++ src/pages/Home.tsx
@@ -0,0 +1,59 @@
+import { useHistory } from 'react-router-dom'
+
+// import { auth, firebase } from '../services/firebase'
+
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import googleIconImg from '../assets/images/google-Icon.svg'
+import '../styles/auth.scss'
+import { Button } from '../components/Button'
+import { useAuth } from '../hooks/useAuth'
+
+
+export function Home() {
+  const history = useHistory()
+  const { user, signInWithGoogle } = useAuth()
+ 
+  
+ async function handleCreateRoom() {
+if (!user) {
+ await signInWithGoogle()
+}
+
+  history.push('/rooms/new')
+}
+    
+   
+  
+  
+  return(
+  
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração" />  
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas de sua audiência em tempo real</p>
+      </aside>
+        <main>
+          <div className="main-content">
+            <img src={logoImg} alt="letmeask" /> 
+            <button onClick={handleCreateRoom} className="create-room"> 
+              <img src={googleIconImg} alt="logo da google" />
+              Crie sua sala com o Google
+            </button>
+            <div className="separator">ou entre em uma sala</div>
+            <form>
+              <input
+               type="text" 
+               placeholder="Digite o código da sala"
+               />
+               <Button type="submit">
+                 Entrar na sala
+               </Button>
+            </form>
+
+          </div>        
+        </main>
+    </div>
+  )
+}
